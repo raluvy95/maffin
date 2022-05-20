d5451ac344df9c50e1ab4f92880a23df52f5a6ac
CatNowBlue <43247023+raluvy95@users.noreply.github.com>
2022-05-20 23:09:31 +0300

    fixed security concern

--- cmds/API/ip.js
+++ cmds/API/ip.js
@@ -7,7 +7,7 @@ module.exports = {
     example: "127.0.0.1",
     run: async (message, args, client) => {
         if(!args[0]) return message.channel.send("Give me the IP.")
-        const jj = await client.f(`https://ipinfo.io/${args[0]}/geo`)
+        const jj = await client.f(`https://ipinfo.io/${encodeURIComponent(args[0])}/geo`)
         const info = await jj.json()
         const e = new client.Embed()
         .setTitle(`IP: ${info.ip}`)
